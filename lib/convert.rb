ceaaa15180d57a82df3ca728263b1adbbacfc837
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-09-06 11:39:11 -0600

    get zero thru ten working with any decimal value

--- lib/convert.rb
+++ lib/convert.rb
@@ -2,96 +2,70 @@
 # => "Two thousand five hundred twenty-three and 04/100 dollars"
 require 'pry'
 class Convert
-  attr_accessor :m
-
-  def money_to_words(a)
-    zero_check(a)
-    decimal_chopper(a)
-    words = ""
-    m.reverse.chars.each_with_index do |char, index|
-      unless char == "0" && m.to_s.reverse.chars[index + 1] != "1"
-        words = include_places(words, index, char)
-      end
-      if index % 3 == 1
-        if char == "1"
-          ten_check(char, a, words, index)
-        else
-          words = dic_up_to_ninety[char] + words
-        end
-      else
-        words = dic_up_to_nine[char] + words
-      end
+  def amount_to_words(a)
+    if a.to_s.include? "."
+      with_decimal(a)
+    else
+      without_decimal(a)
     end
-    print "#{words}#{fraction_check(a)} dollar(s)"
   end
 
-  private
+  def without_decimal(a)
+  end
 
-  def dic_up_to_nine
-    {"0" => "", "1" => "one ", "2" => "two ", "3" => "three ", "4" => "four ", "5" => "five ",
+  def with_decimal(a)
+    cents = a.to_s.reverse[0..1].reverse
+    amount_minus_cents = a.to_s.chop.chop.chop
+    generate_words(amount_minus_cents, cents)
+  end
+
+  def generate_words(amount_minus_cents, cents = nil)
+    phrase = ''
+    amount_minus_cents.chars.reverse.map do |num|
+      phrase << convert_number_to_word(phrase, amount_minus_cents, num)
+    end
+    print "#{phrase}and #{fraction(cents)} dollars"
+  end
+
+  def fraction(cents)
+    "#{cents}/100"
+  end
+
+  def place_value_search(num)
+    #look at where this num is in whole array
+    #set the place_value, depending on the modulo
+    #3 % 4 => 3
+    #3 % 2 => 1
+    #3 % 3 => 0
+  end
+
+  def convert_number_to_word(phrase, amount_minus_cents, num)
+    solo_ten_check(phrase, amount_minus_cents)
+    !phrase.eql?("ten ") ? dictionary_up_to_nine[num] : ""
+  end
+
+  def solo_ten_check(phrase, amount_minus_cents)
+    amount_minus_cents.eql?("10") && !phrase.include?("ten") ? phrase << "ten " : ""
+  end
+
+  def dictionary_up_to_nine
+    {"0" => "zero ", "1" => "one ", "2" => "two ", "3" => "three ", "4" => "four ", "5" => "five ",
      "6" => "six ", "7" => "seven ", "8" => "eight ", "9" => "nine "}
   end
 
-  def dic_up_to_ninety
+  def dictionary_up_to_ninety
     {"0" => "", "1" => "teen", "2" => "twenty ", "3" => "thirty ", "4" => "forty ",
      "5" => "fifty ", "6" => "sixty ", "7" => "seventy ", "8" => "eigthy ", "9" => "ninety "}
   end
 
-  def teens
+  def dictionary_for_teens
     {"one" => "eleven", "two" => "twelve", "three" => "thirteen",
      "four" => "fourteen", "five" => "fifteen", "six" => "sixteen", "seven" => "seventeen",
      "eight" => "eighteen", "nine" => "nineteen"}
   end
 
-  def places
-    ["hundred ", "thousand ", "million "]
-  end
-
-  def zero_check(a)
-    print "zero" if a.to_s.length == 1 && a == 0
-  end
-
-  def fraction_check(a)
-    cents = ""
-    collector = []
-    a.to_s.chars do |char|
-      collector << char
-      cents << char if collector.include? "."
-    end
-    !cents.empty? ? " and " + cents[1..-1] + "/100" : cents = ""
-  end
-
-  def teen_check(words, char)
-    words_arr = words.split
-    words_arr[0] = teens[words_arr[0]]
-    return words_arr.join(' ')
-  end
-
-  def decimal_chopper(a)
-    if a.to_s.chars.include? "."
-      @m = a.to_s.chop.chop.chop
-    else
-      @m = a.to_s
-    end
-  end
-
-  def ten_check(char, a, words, index)
-    if a.to_s.reverse.chars[index - 1] == "0"
-      words = "ten " + words
-    else
-      words = teen_check(words, char)
-    end
-  end
-
-  def include_places(words, index,char)
-    if index % 3 == 2 && char != "0"
-     words = places[0] + words
-    elsif index == 3
-     words = places[1] + words
-    elsif index == 6
-     words = places[2] + words
-    end
-   return words
+  def place_values
+    ["hundred ", "thousand ", "million ", "billion "]
   end
 end
-Convert.new.money_to_words(0)
+Convert.new.amount_to_words("%.2f" % 0.10)
